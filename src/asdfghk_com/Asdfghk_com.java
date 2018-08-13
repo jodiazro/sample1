b3620c897421cb965cdc6b58ed2ecc8d051c4ba6
Estudiante <Estudiante@10.203.3.82>
2018-08-13 08:30:33 -0500

    commit inicial

---
+++ src/asdfghk_com/Asdfghk_com.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asdfghk_com;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Asdfghk_com {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int[] hola=new int[10];
+        for(int i=0;i<10;i++){
+            hola[i]=i+1;
+            System.out.println(hola[i]+1);
+        }
+        
+        
+    }
+    
+}
